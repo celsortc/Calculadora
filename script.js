46da1d6b2e365565f38f687b93568ff6874840a3
Celso <celsortc@outlook.com>
2025-01-04 23:37:36 -0300

    recomeçando js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 let resultado = document.getElementById("resultado");
-let resultadoTemp = document.getElementById("resultadoTemp");
+
 // const botao1 = document.querySelectorAll(".botao1");
 const botao = document.querySelectorAll(".botao");
 let totalTemp = 0;
-let total = 0;
+let resultadotemp = 0;
+let total = 0.0;
+let novaOperacao = false;
+let numeroDigitado = false;
 
 function adicionarNumero(textoBotao) {
-  if (resultado.textContent == "0") {
+  if (numeroDigitado === false) {
     resultado.innerHTML = textoBotao;
+    console.log("ta mundando");
+    numeroDigitado = true;
   } else {
     if (resultado.innerHTML.length >= 12) {
       resultado.innerHTML;
     } else {
       resultado.innerHTML += textoBotao;
+      console.log("ta mundando2");
     }
   }
 }
@@ -21,10 +27,15 @@ function botaoPressionado(event) {
   let botaoPress = event.target; // verifica qual botão foi clicado
   let textoBotao = botaoPress.value; // adiciona o valor do botão na variavel textoBotao
 
-  console.log("botão clicado: ", textoBotao);
+  // console.log("botão clicado: ", textoBotao);
 
-  if ((textoBotao >= 0 && textoBotao <= 9) || textoBotao == ",") {
-    adicionarNumero(textoBotao);
+  if (textoBotao >= 0 && textoBotao <= 9) {
+    if (novaOperacao === true) {
+      resultado.innerHTML = "";
+      adicionarNumero(textoBotao);
+    } else {
+      adicionarNumero(textoBotao);
+    }
   } else {
     verificaOperacao(textoBotao);
   }
@@ -32,29 +43,27 @@ function botaoPressionado(event) {
 
 function verificaOperacao(botaoLido) {
   if (botaoLido === "somar") {
-    totalTemp = resultado.innerHTML;
-    console.log("é uma soma");
-    total = soma(totalTemp);
-    console.log("agora foi", total);
+    numeroDigitado = false;
+
+    resultadotemp = parseFloat(resultado.innerHTML);
+    totalTemp = resultadotemp;
+    // totalTemp = resultadotemp;
+
+    if (novaOperacao === false) {
+      total = soma(totalTemp);
+    } else {
+      resultado.innerHTML = total;
+    }
+
+    totalTemp = 0;
+
+    // resultadotemp = 0;
+    console.log(total, resultadotemp, totalTemp, resultado.innerHTML);
   }
-  //   totalTemp = parseFloat(resultado.innerHTML);
-  //   resultado.innerHTML = "";
-
-  //   // verifica se é um número, se não é ele devolve o erro com window.alert
-  //   if (!isNaN(totalTemp) || totalTemp != 0) {
-  //     console.log("entrou");
-  //     total += totalTemp;
-  //   } else {
-  //     window.alert("Não é um numero, tente digitar novamente");
-  //   }
-
-  //   console.log("numero temporario é:", totalTemp, "total global é:", total);
-  // }
 }
 
 function soma(totalTemp) {
-  console.log(totalTemp);
-  resultado.innerHTML = "";
+  novaOperacao = true;
   return (total += parseFloat(totalTemp));
 }
 
