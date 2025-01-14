161a37c7c61943e5c34d62960ec2b154d84603f1
Celso <celsortc@outlook.com>
2025-01-13 22:35:30 -0300

    restruturando a lógica, e simplificando alguns processos, nesse commit poli a função de adicionar números e lógica por trás

--- script.js
+++ script.js
@@ -1,97 +1,43 @@
 let resultado = document.getElementById("resultado");
 const botao = document.querySelectorAll(".botao");
 
-let primeiroCalculo = true;
-let novaOperacao = false; // para verificar se o número que foi digitado foi pressionado depois de uma operação de soma, subtração, etc..
-let digitouNumero = false; // variavel que verifica se um número foi digitado, para evitar que faça calculos apenas apertando as operações.
-let operadorAtual = null;
-let total = null;
-let totalTemp = 0;
-let num1 = 0;
+total = null;
+primeiroCalculo = true;
 
-function adicionarNumero(textoBotao) {
-  if (primeiroCalculo === true || novaOperacao === true) {
-    //testa se é o primeiro calculo
-    resultado.innerHTML = textoBotao;
-    primeiroCalculo = false;
-    novaOperacao = false;
-  } else if (resultado.innerHTML.length < 12) {
-    // testa se o tamanho do resultado tem menos de 12 caracteres
-    resultado.innerHTML += textoBotao;
-  } else {
-    // se tiver 12 ele não acrescenta mais.
-    resultado.innerHTML;
-  }
-}
-
-function botaoPressionado(event) {
-  let botaoPress = event.target; // verifica qual botão foi clicado
-  let textoBotao = botaoPress.value; // adiciona o valor do botão na variavel textoBotao
-
-  if (textoBotao === ",") {
-    if (resultado.innerHTML.charAt(resultado.innerHTML.length - 1) !== ",") {
-      digitouNumero = true; // passa a variavel para true, indicando que foi um número digitado, nesse caso virgula, mas faz parte do calculo.
-      // aqui ele testa se o último caractere não era uma virgula, se for ele não vai adicionar
-      adicionarNumero(textoBotao); // chama a função que vai adicionar os números no visor da calculadora, alterando o p.resultado
-    }
-  } else if (textoBotao >= 0 && textoBotao <= 9) {
-    digitouNumero = true; // passa a variavel para true, indicando que foi um número digitado.
-    adicionarNumero(textoBotao); // chama a função que vai adicionar os números no visor da calculadora, alterando o p.resultado
-  } else if (textoBotao === "C") {
-    limpaTudo();
-  } else if (textoBotao === "somar") {
-    // se botão clicado for + "somar"
-    if (digitouNumero === true) {
-      total = adicao(); // chama a função que vai somar
-      resultado.innerHTML = total.toString().replace(".", ","); // seta no próprio resultado o valor do total, já formatado no padrão númerico do brasil.
-    }
-  } else if (textoBotao === "subtrair") {
-    num2 = parseFloat(resultado.innerHTML.replace(",", "."));
-    if (total === null) {
-      total = num2;
-      console.log(total, num1, num2);
-      novaOperacao = true;
-      digitouNumero = false;
-      return;
-    }
-    console.log(total, num1, num2);
-
-    resultado.innerHTML = total - num2;
-    total = resultado.innerHTML;
-    console.log(total, num1, num2);
-    novaOperacao = true;
-    digitouNumero = false;
-  }
-}
-
-//
-
-function adicao() {
-  totalTemp = parseFloat(resultado.innerHTML.replace(",", ".")); // passa o valor do resultado para a variavel totalTemp, já convertido em float e substituindo , por .
-  novaOperacao = true;
-  digitouNumero = false; //retorna a variável para false, podendo ser refeitos os calculos.
-
-  if ((total + totalTemp).toString().length <= 12) {
-    //verifica se a soma dos dois números for maior que 12, se for não soma, 12 pq é o escopo máximo (por enquanto)
-    return total + totalTemp;
-  } else {
-    return total;
-  }
-}
+botao.forEach(clicou);
 
 function limpaTudo() {
-  resultado.innerHTML = "0";
-  primeiroCalculo = true;
-  novaOperacao = false;
-  totalTemp = 0;
-  total = 0;
-  num2 = 0;
-  operadorAtual = null;
+  total = null;
+  console.log("resetou");
 }
 
 function clicou(teste) {
-  teste.addEventListener("click", botaoPressionado);
+  teste.addEventListener("click", verificaBotao);
 }
 
-botao.forEach(clicou);
-// window.alert("Não pode adicionar outra virgula nessa operação.")
+function verificaBotao(event) {
+  let textoBotao = event.target.value; // verifica qual botão foi clicado e já pega o valor dele
+
+  if (textoBotao === "," && resultado.innerHTML.indexOf(",") === -1) {
+    //simplificação da lógica, em que ele varre a string buscando "," caso encontre, ele não permite a adição de nenhuma "," em qualquer circunstacia
+    exibeNumero(textoBotao);
+    // return;
+  } else if (textoBotao >= 0 || textoBotao <= 9) {
+    // se for um número entra para ser exibido na tela, e se for uma operação, irá verificar qual é
+    exibeNumero(textoBotao);
+  } else {
+    verificaOperacao(textoBotao);
+  }
+}
+
+function exibeNumero(textoBotao) {
+  if (primeiroCalculo === true) {
+    //condicional pra verificar se é o primeiro calculo depois da calculadora resetada, soluciona o problema de somar os numeros ao 0;
+    resultado.innerHTML = textoBotao;
+    primeiroCalculo = false;
+  } else if (primeiroCalculo === false && resultado.innerHTML.length < 12) {
+    resultado.innerHTML += textoBotao;
+  }
+}
+
+function verificaOperacao() {}
---
+++ script1.0.js
@@ -0,0 +1,100 @@
+let resultado = document.getElementById("resultado");
+const botao = document.querySelectorAll(".botao");
+
+let primeiroCalculo = true;
+let novaOperacao = false; // para verificar se o número que foi digitado foi pressionado depois de uma operação de soma, subtração, etc..
+let digitouNumero = false; // variavel que verifica se um número foi digitado, para evitar que faça calculos apenas apertando as operações.
+let operadorAtual = null;
+let total = null;
+let totalTemp = 0;
+let num1 = 0;
+
+function adicionarNumero(textoBotao) {
+  if (primeiroCalculo === true || novaOperacao === true) {
+    //testa se é o primeiro calculo
+    resultado.innerHTML = textoBotao;
+    novaOperacao = false;
+    primeiroCalculo = false;
+  } else if (resultado.innerHTML.length < 12) {
+    // testa se o tamanho do resultado tem menos de 12 caracteres
+    resultado.innerHTML += textoBotao;
+  } else {
+    // se tiver 12 ele não acrescenta mais.
+    resultado.innerHTML;
+  }
+}
+
+function botaoPressionado(event) {
+  let botaoPress = event.target; // verifica qual botão foi clicado
+  let textoBotao = botaoPress.value; // adiciona o valor do botão na variavel textoBotao
+
+  if (textoBotao === ",") {
+    if (resultado.innerHTML.charAt(resultado.innerHTML.length - 1) !== ",") {
+      digitouNumero = true; // passa a variavel para true, indicando que foi um número digitado, nesse caso virgula, mas faz parte do calculo.
+      // aqui ele testa se o último caractere não era uma virgula, se for ele não vai adicionar
+      adicionarNumero(textoBotao); // chama a função que vai adicionar os números no visor da calculadora, alterando o p.resultado
+    }
+  } else if (textoBotao >= 0 && textoBotao <= 9) {
+    digitouNumero = true; // passa a variavel para true, indicando que foi um número digitado.
+    adicionarNumero(textoBotao); // chama a função que vai adicionar os números no visor da calculadora, alterando o p.resultado
+  } else if (textoBotao === "C") {
+    limpaTudo();
+  } else if (textoBotao === "somar") {
+    // se botão clicado for + "somar"
+    if (digitouNumero === true) {
+      total = adicao(); // chama a função que vai somar
+      resultado.innerHTML = total.toString().replace(".", ","); // seta no próprio resultado o valor do total, já formatado no padrão númerico do brasil.
+    }
+  } else if (textoBotao === "subtrair") {
+    num2 = parseFloat(resultado.innerHTML.replace(",", "."));
+    if (total === null) {
+      total = num2;
+      console.log(total, num1, num2);
+      novaOperacao = true;
+      digitouNumero = false;
+      return;
+    }
+    console.log(total, num1, num2);
+
+    resultado.innerHTML = total - num2;
+    total = resultado.innerHTML;
+    console.log(total, num1, num2);
+    novaOperacao = true;
+    digitouNumero = false;
+  }
+}
+
+//
+
+function adicao() {
+  totalTemp = parseFloat(resultado.innerHTML.replace(",", ".")); // passa o valor do resultado para a variavel totalTemp, já convertido em float e substituindo , por .
+  novaOperacao = true;
+  digitouNumero = false; //retorna a variável para false, podendo ser refeitos os calculos.
+
+  if ((total + totalTemp).toString().length <= 12) {
+    //verifica se a soma dos dois números for maior que 12, se for não soma, 12 pq é o escopo máximo (por enquanto)
+    return total + totalTemp;
+  } else {
+    return total;
+  }
+}
+
+function limpaTudo() {
+  resultado.innerHTML = "0";
+  novaOperacao = false;
+  totalTemp = 0;
+  num2 = 0;
+  operadorAtual = null;
+  primeiroCalculo = true;
+  digitouNumero = false;
+  operadorAtual = null;
+  total = null;
+  num1 = 0;
+}
+
+function clicou(teste) {
+  teste.addEventListener("click", botaoPressionado);
+}
+
+botao.forEach(clicou);
+// window.alert("Não pode adicionar outra virgula nessa operação.")
